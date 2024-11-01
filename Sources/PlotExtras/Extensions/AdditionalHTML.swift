4ab413af891602ddbe5b14ffdef443adbc7691bf
Matt Gacy <matt@mgacy.com>
2024-11-01 14:05:42 -0700

    Additional element components

--- Sources/PlotExtras/Extensions/AdditionalHTML.swift
+++ Sources/PlotExtras/Extensions/AdditionalHTML.swift
@@ -13,6 +13,22 @@ import Foundation
 
 public extension ElementDefinitions {
     /// Definition for the `<i>` element.
+    enum Address: ElementDefinition {
+        public typealias InputContext = HTML.BodyContext
+        public typealias OutputContext = HTML.BodyContext
+
+        public static let wrapper = Node.address
+    }
+
+    /// Definition for the `<hgroup>` element.
+    enum HeaderGroup: ElementDefinition {
+        public typealias InputContext = HTML.BodyContext
+        public typealias OutputContext = HTML.BodyContext
+
+        public static let wrapper = Node.hgroup
+    }
+
+    /// Definition for the `<i>` element.
     enum Idiomatic: ElementDefinition {
         public typealias InputContext = HTML.BodyContext
         public typealias OutputContext = HTML.BodyContext
@@ -27,10 +43,27 @@ public extension ElementDefinitions {
 
         public static let wrapper = Node.main
     }
+
+    /// Definition for the `<search>` element.
+    enum Search: ElementDefinition {
+        public typealias InputContext = HTML.BodyContext
+        public typealias OutputContext = HTML.BodyContext
+
+        public static let wrapper = Node.search
+    }
 }
+
+/// A container component that's rendered using the `<i>` element.
+public typealias Address = ElementComponent<ElementDefinitions.Address>
+
+/// A container component that's rendered using the `<i>` element.
+public typealias HeaderGroup = ElementComponent<ElementDefinitions.HeaderGroup>
 
 /// A container component that's rendered using the `<i>` element.
 public typealias Idiomatic = ElementComponent<ElementDefinitions.Idiomatic>
 
 /// A container component that's renered using the `<main>` element.
 public typealias Main = ElementComponent<ElementDefinitions.Main>
+
+/// A container component that's renered using the `<search>` element.
+public typealias Search = ElementComponent<ElementDefinitions.Search>
