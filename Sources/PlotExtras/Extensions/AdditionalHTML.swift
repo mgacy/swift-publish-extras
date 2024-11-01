a7dd26b4c97369980d9a29d52cafe0b9d00d463b
Matt Gacy <matt@mgacy.com>
2024-11-01 13:37:32 -0700

    Add Main element component

--- Sources/PlotExtras/Extensions/AdditionalHTML.swift
+++ Sources/PlotExtras/Extensions/AdditionalHTML.swift
@@ -19,7 +19,18 @@ public extension ElementDefinitions {
 
         public static let wrapper = Node.i
     }
+
+    /// Definition for the `<main>` element.
+    enum Main: ElementDefinition {
+        public typealias InputContext = HTML.BodyContext
+        public typealias OutputContext = HTML.BodyContext
+
+        public static let wrapper = Node.main
+    }
 }
 
 /// A container component that's rendered using the `<i>` element.
 public typealias Idiomatic = ElementComponent<ElementDefinitions.Idiomatic>
+
+/// A container component that's renered using the `<main>` element.
+public typealias Main = ElementComponent<ElementDefinitions.Main>
